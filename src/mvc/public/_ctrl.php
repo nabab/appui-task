4b4762896aa5a1272471fdad1ee440f3b59b5d5b
nabab <thomas.nabet@gmail.com>
2018-03-16 14:20:46 +0100

    Tests with languages

--- src/mvc/public/_ctrl.php
+++ src/mvc/public/_ctrl.php
@@ -15,11 +15,17 @@
 // after with -j to j0oin with existing
 // find ../../../mvc -iname "*.php" | xargs xgettext -d appui_task -j
 // msgfmt -o appui_task.mo appui_task.po
+clearstatcache();
 if ( !\defined('APPUI_TASKS_ROOT') ){
   define('APPUI_TASKS_ROOT', $ctrl->plugin_url('appui-task').'/');
 }
-bindtextdomain('appui_task', BBN_LIB_PATH.'bbn/appui-task/src/locale');
-setlocale(LC_ALL, "fr_FR.utf8");
-textdomain('appui_task');
+
+$domain = 'task';
+
+bindtextdomain($domain, BBN_LIB_PATH.'bbn/appui-task/src/locale');
+setlocale(LC_MESSAGES, "fr_FR.utf8");
+textdomain($domain);
+
+
 
 return 1;
---
+++ src/mvc/public/test.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Describe what it does!
+ *
+ **/
+
+/** @var $this \bbn\mvc\controller */
+clearstatcache();
+textdomain('task');
+
+echo _("User");
