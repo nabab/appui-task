04cefc7f572a5ad41b4a770cc935ba210623dff5
Mirko Argentino <mirko@bbn.so>
2018-04-09 19:52:05 +0200

    User avatar fix
    User avatar fix

--- src/mvc/html/templates/appui-tasks-user-avatar.php
+++ src/mvc/html/templates/appui-tasks-user-avatar.php
@@ -1,4 +1,5 @@
-<bbn-initial :user-id="source.id_user"
+<bbn-initial v-if="source.id_user"
+             :user-id="source.id_user"
              :title="userName(source.id_user)"
              :height="25"
              :width="25"
